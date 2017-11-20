9bee93adf0f180b91643b09356c96a1393208064
illiptic <jeremy.davis42@gmail.com>
2017-11-20 12:14:33 +0900

    Fix build

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core'
 import { Headers, Http } from '@angular/http'
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment'
 
-const baseUrl = '/api/'
+const baseUrl = environment.apiUrl + '/'
 const key = '5129EB54-8EB1-3941-95FF-BF1826D6309F615A7A1B-52D6-423E-97E7-0450B0C25307'
 
 const headers = new Headers({
